88ac87b5e7ca64545fadd765535632cd876d954d
Salihu Abdulhamid <salihu45@gmail.com>
2023-04-30 10:56:27 +0100

    feat: 🎸 Empty message screen and others
    Empty message screen, attachment and send button appearing correctly,
    text container appearing in project, removal of today button, increase
    of search button

--- Admin-Dashboard-SVP/src/layout/messages/MessageDashboard.jsx
+++ Admin-Dashboard-SVP/src/layout/messages/MessageDashboard.jsx
@@ -12,59 +12,57 @@ import { useForm } from "react-hook-form";
 
 const MessageDashboard = () => {
   // const [id, setId] = useState("");
-  const { data: allMessages } = useGetAllMessagesQuery({
+  // fetching users and details about them
+  const { data: allMessagesDetails } = useGetAllMessagesQuery({
     refetchOnMountArgChange: true,
   });
-
+  // sending messages
   const [updateMsg] = useAddMessagesMutation();
 
-  const messagethreads = allMessages || [];
+  // preventing code break. displays empty array till the messages loas
+  const messageDetails = allMessagesDetails || [];
 
-  console.log(messagethreads);
+  // getting the first object in the array of objects and copying it into a variable
+  var first = [...messageDetails].shift();
 
-  console.log(messagethreads[0]);
-
-  var first = [...messagethreads].shift();
-
+  // declaring new array
   var newArray = [];
 
+  // pushing the object into a new array
   newArray.push(first);
 
-  console.log(newArray);
-
-  const initialValue = newArray[0]?._id;
-
-  console.log(initialValue);
-
+  // logic of line 32 was done so as to return an array cause using an index will return an object which can't be mapped. directly using the object will involve multiple containers
+  // filter states
   const [filter, setFilter] = useState();
 
-  console.log(filter);
-
-  // setFilter(newArray[0].id);
-
+  // useMemo hook to filter message details according to each user
   const data = useMemo(() => {
-    if (!filter) return newArray;
-    const filteredData = messagethreads.filter((item) => item._id === filter);
+    if (!filter) return [];
+    const filteredData = messageDetails.filter((item) => item._id === filter);
     return filteredData;
-  }, [filter, messagethreads]);
+  }, [filter, messageDetails]);
 
+  // fetching messages. filter contains the id. line 112, when you click it saves the id in a filter
   const { data: allChats, refetch } = useGetAllChatsQuery(filter);
 
+  // prevents code breaks
   const chats = allChats || [];
 
+  console.log(chats);
+
+  // useForm hook
   const { register, control, reset, handleSubmit } = useForm();
 
+  //closing of messages that involves group chats
   const [open, setOpen] = useState(true);
 
-  // console.log(msg);
-  // console.log(socket);
-  console.log(filter);
+  // form hook submission of messages
   const submitForm = async (msg) => {
-    console.log("batman");
     reset();
-    console.log(msg);
     updateMsg({ data: msg, id: filter }).unwrap();
   };
+
+  // useEffect to refresh every 1seconds to check for new mwssages
   useEffect(() => {
     const interval = setInterval(() => {
       refetch();
@@ -72,6 +70,8 @@ const MessageDashboard = () => {
 
     return () => clearInterval(interval);
   }, []);
+
+  const [defaultState, setDefaultState] = useState(true);
 
   return (
     <Container className={message.container}>
@@ -91,18 +91,10 @@ const MessageDashboard = () => {
                     className={message.searchicon}
                   />
                 </div>
-                <div className={message.datecontainer}>
-                  <div className={message.absolutecenter}>
-                    <p className={message.date}>Today</p>
-                  </div>
-                  <div className={message.absolutecenter}>
-                    <Image src="/icons/arrow-down-yellow.svg" alt="icons" />
-                  </div>
-                </div>
               </div>
               <div className={message.userslistcontainer}>
-                {messagethreads.map((messagethread, index) => {
-                  const active = filter === messagethread._id;
+                {messageDetails.map((messageDetail, index) => {
+                  const active = filter === messageDetail._id;
                   return (
                     <div
                       className={
@@ -112,14 +104,16 @@ const MessageDashboard = () => {
                       }
                       key={index}
                       filter={newArray[0]._id}
-                      onClick={() => setFilter(messagethread._id)}
+                      onClick={() => {
+                        setFilter(messageDetail._id), setDefaultState(false);
+                      }}
                     >
                       <div
                         flex={message.userinfocontainer}
                         style={{ display: "flex", gap: "1rem" }}
                       >
                         <img
-                          src={`${messagethread.avatar}`}
+                          src={`${messageDetail.avatar}`}
                           style={{ width: "40px", height: "36px" }}
                           alt="avatar"
                         />
@@ -129,7 +123,7 @@ const MessageDashboard = () => {
                               active ? message.usernameactive : message.username
                             }
                           >
-                            {messagethread.admin.name}
+                            {messageDetail.admin.name}
                           </p>
                           {/* <p className={message.time}>Just Now</p> */}
                         </div>
@@ -161,64 +155,99 @@ const MessageDashboard = () => {
                     })}
                   </div>
                 </div>
+                {defaultState ? (
+                  <div className={message.chatcontainerempty}>
+                    <p className={message.chatempty}>No Messages Selected</p>
+                  </div>
+                ) : null}
                 {data.map((project, index) => {
                   return project?.project ? (
-                    <div key={index} className={message.chatcontainer}>
-                      {open ? (
-                        <div className={message.detailsmessagecontainer}>
-                          <Image
-                            src="/icons/close.svg"
-                            className={message.close}
-                            alt="close"
-                            onClick={() => setOpen(false)}
-                          />
-                          <div className={message.messagedetailscontainer}>
-                            <p className={message.messagetitle}>Project:</p>
-                            <p className={message.messagedescription1}>
-                              Skyline Building Construction
-                            </p>
-                          </div>
+                    <>
+                      <div key={index} className={message.chatcontainer}>
+                        {open ? (
+                          <div className={message.detailsmessagecontainer}>
+                            <Image
+                              src="/icons/close.svg"
+                              className={message.close}
+                              alt="close"
+                              onClick={() => setOpen(false)}
+                            />
+                            <div className={message.messagedetailscontainer}>
+                              <p className={message.messagetitle}>Project:</p>
+                              <p className={message.messagedescription1}>
+                                Skyline Building Construction
+                              </p>
+                            </div>
 
-                          <div className={message.messagedetailscontainer}>
-                            <p className={message.messagetitle}>Status:</p>
-                            <p className={message.messagedescription}>
-                              In Progress
-                            </p>
+                            <div className={message.messagedetailscontainer}>
+                              <p className={message.messagetitle}>Status:</p>
+                              <p className={message.messagedescription}>
+                                In Progress
+                              </p>
+                            </div>
+                            <div className={message.messagedetailscontainer}>
+                              <p className={message.messagetitle}>Due Date:</p>
+                              <p className={message.messagedescription}>
+                                <Image
+                                  src="/icons/calendar.svg"
+                                  alt="calendar"
+                                  style={{
+                                    marginRight: "0.2rem",
+                                    width: "15px",
+                                    height: "15px",
+                                  }}
+                                />
+                                18 Aug 2023
+                              </p>
+                            </div>
+                            <div className={message.messagedetailscontainer}>
+                              <p className={message.messagetitle}>
+                                Description:
+                              </p>
+                              <p className={message.messagedescription}>
+                                Lorem ipsum dolor sit amet consectetur. Lectus
+                                le leo enim quis facilisis. Elit ut facilisi
+                                arcu nibh. Etia posuere posuere rhoncus nam.
+                                Molestie lorem qui id sed quis eu etiam commodo.
+                              </p>
+                            </div>
                           </div>
-                          <div className={message.messagedetailscontainer}>
-                            <p className={message.messagetitle}>Due Date:</p>
-                            <p className={message.messagedescription}>
-                              <Image
-                                src="/icons/calendar.svg"
-                                alt="calendar"
-                                style={{
-                                  marginRight: "0.2rem",
-                                  width: "15px",
-                                  height: "15px",
-                                }}
-                              />
-                              18 Aug 2023
-                            </p>
-                          </div>
-                          <div className={message.messagedetailscontainer}>
-                            <p className={message.messagetitle}>Description:</p>
-                            <p className={message.messagedescription}>
-                              Lorem ipsum dolor sit amet consectetur. Lectus le
-                              leo enim quis facilisis. Elit ut facilisi arcu
-                              nibh. Etia posuere posuere rhoncus nam. Molestie
-                              lorem qui id sed quis eu etiam commodo.
-                            </p>
-                          </div>
+                        ) : null}
+                      </div>
+                      <form onSubmit={handleSubmit(submitForm)}>
+                        <div className={message.inputcontainer}>
+                          <Image
+                            className={message.attachment}
+                            src="/icons/attachment.svg"
+                            alt="attach"
+                          />
+                          <Form.Group controlId="exampleForm.ControlTextarea1">
+                            <Form.Control
+                              as="textarea"
+                              className={message.textarea}
+                              {...register("message")}
+                              // value={msg}
+                              placeholder=""
+                            />
+                          </Form.Group>
+                          <button className={message.sendbutton} type="submit">
+                            <Image
+                              type="submit"
+                              // onClick={handleMessage}
+                              src="/icons/send1.svg"
+                              className={message.attachmentsend}
+                              alt="send"
+                            />
+                          </button>
                         </div>
-                      ) : null}
-                    </div>
+                      </form>
+                    </>
                   ) : (
                     <>
                       <div className={message.chatcontainer}>
                         {/* <div style={{ display: "flex" }}> */}
                         <div style={{ alignSelf: "flex-end", width: "100%" }}>
                           {chats.map((chat, index) => {
-                            console.log(chat);
                             return (
                               <div>
                                 <div key={index}>
